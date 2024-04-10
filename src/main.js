09a4e8418e1bd1de111bc6248347e9d5f85cc9e3
vulgobl <bernardo683@gmail.com>
2024-04-10 20:39:48 -0300

    exercicios 1 e 2 feitos

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 // const nomes = ["bernardo", "pedro", "jose", "fabiana", "gabriel" ]
 // for (let nome = 0; nome < nomes.length; nome += 1) {
 //     console.log(nomes[nome]);
@@ -15,7 +14,7 @@
 // console.log(ePrimo
 
 // const salario = 3000;// salario bruto
-// let salarioLiquido;
+// let salarioLiquido; 
 // if (salario <= 1556.94) {
 //     salarioLiquido = salario - (salario / 100) * 8;
 // } else if (salario >= 1556.95 && salario <= 2594.92) {
@@ -37,3 +36,23 @@
 //     salarioLiquido -= (salarioLiquido / 100) * 27.5 - 869.36;
 // }
 // console.log(salarioLiquido);
+// Exercício 1
+// Considere esse array para realizar os próximos exercícios.
+
+// Some todos os valores contidos no array e imprima o resultado.
+// Calcule e imprima a média aritmética dos valores contidos no array.
+// Com base no código que acabou de gerar, referente ao cálculo da média aritmética, faça com que:
+// caso o valor final seja maior que 20, imprima a mensagem “O valor da média aritmética é maior que 20”;
+// caso não seja maior que 20, imprima a mensagem “O valor da média aritmética é menor ou igual a 20”.
+// Descubra o maior valor contido no array e imprima-o.
+// Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: “Nenhum valor ímpar encontrado”.
+
+const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// for (let numero = 0; numero < numbers.length; numero += 1) {
+//     console.log(numbers[numero]);
+// }
+// let soma = 0;
+// for (let indice = 0; indice < numbers.length; indice += 1) {
+//     soma += numbers[indice];
+// }
+// console.log(soma);
