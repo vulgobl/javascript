3b79c541dddcc605a7593ef881bc4416b4c1a412
vulgobl <bernardo683@gmail.com>
2024-04-10 18:17:46 -0300

    fundamento 1 concluido

---
+++ src/main.js
@@ -0,0 +1,39 @@
+/* eslint-disable no-unused-vars */
+// const nomes = ["bernardo", "pedro", "jose", "fabiana", "gabriel" ]
+// for (let nome = 0; nome < nomes.length; nome += 1) {
+//     console.log(nomes[nome]);
+// }
+
+// const primos = 10000000000000000000
+// let ePrimo = true
+// for (let primo = 2; primo < primos; primo += 1) {
+//     if (primos % primo === 0) {
+//         ePrimo = false
+//         break
+//     } 
+// }
+// console.log(ePrimo
+
+// const salario = 3000;// salario bruto
+// let salarioLiquido;
+// if (salario <= 1556.94) {
+//     salarioLiquido = salario - (salario / 100) * 8;
+// } else if (salario >= 1556.95 && salario <= 2594.92) {
+//     salarioLiquido = salario - (salario / 100) * 9;
+// } else if (salario >= 2594.93 && salario <= 5189.82) {
+//     salarioLiquido = salario - (salario / 100) * 11;
+// } else if (salario > 5189.82) {
+//     salarioLiquido = salario - 570.88;
+// }
+// if (salarioLiquido <= 1903.98) {
+//     salarioLiquido -= 0; 
+// } else if (salarioLiquido >= 1903.99 && salarioLiquido <= 2826.65) {
+//     salarioLiquido -= (salarioLiquido / 100) * 7.5 - 142.80;
+// } else if (salarioLiquido >= 2826.66 && salarioLiquido <= 3751.05) {
+//     salarioLiquido -= (salarioLiquido / 100) * 15 - 354.80;
+// } else if (salarioLiquido >= 3751.06 && salarioLiquido <= 4664.68) {
+//     salarioLiquido -= (salarioLiquido / 100) * 22.5 - 636.13;
+// } else if (salarioLiquido >= 4664.68) {
+//     salarioLiquido -= (salarioLiquido / 100) * 27.5 - 869.36;
+// }
+// console.log(salarioLiquido);
