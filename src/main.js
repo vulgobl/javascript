196ebe15edcec33db3bc35fef8ad0d3d0ce0fa00
Pedro Costa <pedrocosta012.dev@gmail.com>
2024-04-17 19:25:06 -0300

    aula de comparaçao

--- src/main.js
+++ src/main.js
@@ -9,12 +9,12 @@
 //     if (primos % primo === 0) {
 //         ePrimo = false
 //         break
-//     } 
+//     }
 // }
 // console.log(ePrimo
 
 // const salario = 3000;// salario bruto
-// let salarioLiquido; 
+// let salarioLiquido;
 // if (salario <= 1556.94) {
 //     salarioLiquido = salario - (salario / 100) * 8;
 // } else if (salario >= 1556.95 && salario <= 2594.92) {
@@ -25,7 +25,7 @@
 //     salarioLiquido = salario - 570.88;
 // }
 // if (salarioLiquido <= 1903.98) {
-//     salarioLiquido -= 0; 
+//     salarioLiquido -= 0;
 // } else if (salarioLiquido >= 1903.99 && salarioLiquido <= 2826.65) {
 //     salarioLiquido -= (salarioLiquido / 100) * 7.5 - 142.80;
 // } else if (salarioLiquido >= 2826.66 && salarioLiquido <= 3751.05) {
@@ -39,20 +39,33 @@
 // Exercício 1
 // Considere esse array para realizar os próximos exercícios.
 
-// Some todos os valores contidos no array e imprima o resultado.
-// Calcule e imprima a média aritmética dos valores contidos no array.
-// Com base no código que acabou de gerar, referente ao cálculo da média aritmética, faça com que:
-// caso o valor final seja maior que 20, imprima a mensagem “O valor da média aritmética é maior que 20”;
+// Some todos os valores contidos no array e imprima o resultado.//pronto
+// Calcule e imprima a média aritmética dos valores contidos no array.//pronto
+// Com base no código que acabou de gerar, referente ao cálculo da média aritmética, faça com que://pronto
+// caso o valor final seja maior que 20, imprima a mensagem “O valor da média aritmética é maior que 20”;//pronto
 // caso não seja maior que 20, imprima a mensagem “O valor da média aritmética é menor ou igual a 20”.
 // Descubra o maior valor contido no array e imprima-o.
 // Descubra quantos valores ímpares existem no array e imprima o resultado. Caso não exista nenhum, imprima a mensagem: “Nenhum valor ímpar encontrado”.
 
 const numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
 // for (let numero = 0; numero < numbers.length; numero += 1) {
 //     console.log(numbers[numero]);
-// }
+// }//[0]
+
+let maiorNumero = 0;
+for (let indice = 0; indice < numbers.length; indice += 1) {
+    maiorNumero += numbers[indice];
+    const c = numbers[indice];
+    console.log(c);
+}
 // let soma = 0;
 // for (let indice = 0; indice < numbers.length; indice += 1) {
 //     soma += numbers[indice];
 // }
-// console.log(soma);
+// const media = soma / numbers.length;
+// if (media > 20) {
+//     console.log('o valor da media aritimetica e maior que 20');
+// } else {
+// console.log('o valor da media aritimetica e menor ou igual a 20');
+// }
