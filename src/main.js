e85d8bd790e570b47d548232d5c1d1c07eba2778
vulgobl <bernardo683@gmail.com>
2024-09-27 18:47:59 -0300

    obj [1]

--- src/main.js
+++ src/main.js
@@ -187,3 +187,26 @@
 // }
 
 // console.log(returnVezes(['oi', 'Hop!!!', 'tudo bem', 'tudo bem', 'mundo', 'tudo bem', '11'])); // 2
+
+const reader = {
+    name: 'Julia',
+    lastName: 'Pessoa',
+    age: 21,
+    favoriteBooks: [
+        {
+            title: 'O Senhor dos Anéis - a Sociedade do Anel',
+            author: 'J. R. R. Tolkien',
+            publisher: 'Martins Fontes',
+        },
+    ],
+};
+
+console.log(
+    `O livro favorito de ${
+        reader.name
+    } ${
+        reader.lastName
+    } se chama "${
+        reader.favoriteBooks[0].title
+    }"`,
+);
