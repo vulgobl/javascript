16ee68c8d4147d8b4dba6568f43e7e570ed4f105
vulgobl <bernardo683@gmail.com>
2025-02-26 19:26:17 -0300

    O FINAL DO DA LISTA PEDRO E O INICIO QUE NUNCA ACABA!

--- src/main.js
+++ src/main.js
@@ -312,3 +312,49 @@
 
 // console.log(hasKeyInAll(school, 'course'));
 // // string 'algo'
+
+// ██████╗  ██████╗     ██╗ ██████╗ ██████╗     ██╗██████╗ ███████╗
+// ╚════██╗██╔════╝    ██╔╝██╔═████╗╚════██╗   ██╔╝╚════██╗██╔════╝
+// █████╔╝███████╗   ██╔╝ ██║██╔██║ █████╔╝  ██╔╝  █████╔╝███████╗
+// ██╔═══╝ ██╔═══██╗ ██╔╝  ████╔╝██║██╔═══╝  ██╔╝  ██╔═══╝ ╚════██║
+// ███████╗╚██████╔╝██╔╝   ╚██████╔╝███████╗██╔╝   ███████╗███████║
+// ╚══════╝ ╚═════╝ ╚═╝     ╚═════╝ ╚══════╝╚═╝    ╚══════╝╚══════╝
+
+let frutas = [
+    'Melancia', 'Abacate', 'Melancia', 'Melancia', 'Uva', 'Laranja',
+    'Jaca', 'Pera', 'Melancia', 'Uva', 'Laranja', 'Melancia',
+    'Banana', 'Uva', 'Pera', 'Abacate', 'Laranja', 'Abacate',
+    'Banana', 'Melancia', 'Laranja', 'Laranja', 'Jaca', 'Uva',
+    'Banana', 'Uva', 'Laranja', 'Pera', 'Melancia', 'Uva',
+    'Jaca', 'Banana', 'Pera', 'Abacate', 'Melancia', 'Melancia',
+    'Laranja', 'Pera', 'Banana', 'Jaca', 'Laranja', 'Melancia',
+    'Abacate', 'Abacate', 'Pera', 'Melancia', 'Banana', 'Banana',
+    'Abacate', 'Uva', 'Laranja', 'Banana', 'Abacate', 'Uva',
+    'Uva', 'Abacate', 'Abacate', 'Melancia', 'Uva', 'Jaca',
+    'Uva', 'Banana', 'Abacate', 'Banana', 'Uva', 'Banana',
+    'Laranja', 'Laranja', 'Jaca', 'Jaca', 'Abacate', 'Jaca',
+    'Laranja', 'Melancia', 'Pera', 'Jaca', 'Melancia', 'Uva',
+    'Abacate', 'Jaca', 'Jaca', 'Abacate', 'Uva', 'Laranja',
+    'Pera', 'Melancia', 'Jaca', 'Pera', 'Laranja', 'Jaca',
+    'Pera', 'Melancia', 'Jaca', 'Banana', 'Laranja', 'Jaca',
+    'Banana', 'Pera', 'Abacate', 'Uva',
+];
+const frutasQuantidade = {};
+
+for (let i = 0; i < frutas.length; i += 1) {
+    if (!(frutas[i] in frutasQuantidade)) {
+        frutasQuantidade[frutas[i]] = 0;
+    }
+    frutasQuantidade[frutas[i]] += 1;
+}
+console.log(frutasQuantidade);
+
+/*
+██████╗  ██████╗     ██╗ ██████╗ ██████╗     ██╗██████╗  ██████╗ ██████╗ ██╗  ██╗     ██╗ █████╗    ██████╗ ██████╗    ██████╗  ██████╗     ███████╗██╗███╗   ███╗
+╚════██╗██╔════╝    ██╔╝██╔═████╗╚════██╗   ██╔╝╚════██╗██╔═████╗╚════██╗██║  ██║    ███║██╔══██╗██╗╚════██╗╚════██╗██╗╚════██╗██╔═████╗    ██╔════╝██║████╗ ████║
+ █████╔╝███████╗   ██╔╝ ██║██╔██║ █████╔╝  ██╔╝  █████╔╝██║██╔██║ █████╔╝███████║    ╚██║╚██████║╚═╝ █████╔╝ █████╔╝╚═╝ █████╔╝██║██╔██║    █████╗  ██║██╔████╔██║
+██╔═══╝ ██╔═══██╗ ██╔╝  ████╔╝██║██╔═══╝  ██╔╝  ██╔═══╝ ████╔╝██║██╔═══╝ ╚════██║     ██║ ╚═══██║██╗██╔═══╝ ██╔═══╝ ██╗██╔═══╝ ████╔╝██║    ██╔══╝  ██║██║╚██╔╝██║
+███████╗╚██████╔╝██╔╝   ╚██████╔╝███████╗██╔╝   ███████╗╚██████╔╝███████╗     ██║     ██║ █████╔╝╚═╝███████╗███████╗╚═╝███████╗╚██████╔╝    ██║     ██║██║ ╚═╝ ██║
+╚══════╝ ╚═════╝ ╚═╝     ╚═════╝ ╚══════╝╚═╝    ╚══════╝ ╚═════╝ ╚══════╝     ╚═╝     ╚═╝ ╚════╝    ╚══════╝╚══════╝   ╚══════╝ ╚═════╝     ╚═╝     ╚═╝╚═╝     ╚═╝
+*/
+                                                                                                                                                              
